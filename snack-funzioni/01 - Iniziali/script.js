ad2ff594820b5ab81001bc9e7bf3c5d851972091
MarcelloPescatore <pescatoremarcello@gmail.com>
2024-10-07 15:26:13 +0200

    complete the first assignment

--- snack-funzioni/01 - Iniziali/script.js
+++ snack-funzioni/01 - Iniziali/script.js
@@ -2,12 +2,18 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
-
+function myFunction(array){
+    const result = []
+    for (let i = 0; i < array.length; i++){
+        let firstChar = array[i][0]
+        result.push(firstChar)
+    }
+    return result
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+let initials = myFunction(names)
+console.log(initials);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
