93d558a458cb94fa7336e7067b435da15908294c
MarcelloPescatore <pescatoremarcello@gmail.com>
2024-10-07 16:30:15 +0200

    complete the third assignment

--- snack-funzioni/03 - conta vocali/script.js
+++ snack-funzioni/03 - conta vocali/script.js
@@ -4,10 +4,19 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+function myFunction(string){
+    let counter = 0;
+    const vowels = 'aeiou'
+    for (let i = 0; i < word.length; i++){
+        if (vowels.includes(string[i])){
+            counter += 1 ;
+        }
+    }
+    return counter
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+let vowelsCount = myFunction(word)
+console.log(vowelsCount);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
